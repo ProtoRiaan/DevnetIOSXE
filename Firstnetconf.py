5e543fdbc3e0155b85cb9e31b4db86ec1a313e43
Riaan Schuld <rhschuld@gmail.com>
2023-03-13 01:02:11 +0000

    Feel like I am hitting a wall trying to create an loopback interface from scratch. Checking on the interface type capability now

--- Firstnetconf.py
+++ Firstnetconf.py
@@ -1,6 +1,7 @@
 from ncclient import manager
-import lxml.etree as ET
+#import lxml.etree as ET
 import xmltodict
+import xml.dom.minidom as MiniDom
 
 router={"host":"sandbox-iosxe-latest-1.cisco.com", "port":"830",
         "username":"developer","password":"redmonitorioxdedicated34721"}
@@ -10,21 +11,33 @@ datafilter ="""
   </interfaces>
   """
 #start the netconf session
-m=manager.connect(host=router["host"],port=router['port'],username=router["username"],password=router["password"],hostkey_verify=False,device_params={'name':'iosxe'})
+m=manager.connect(host=router["host"],port=router['port'],username=router["username"],password=router["password"],hostkey_verify=False,device_params={'name':'csr'})
 
-#grab and print capabilities
+#  grab and print capabilities to file and screen
+
+#capabilitiesFile = open('Capabilities.txt',"a")
+
 for capability in m.server_capabilities:
-        print('*' * 50)
+        print('*' * 50,)
         print(capability)
+       # capabilitiesFile.write('*' * 50)
+       # capabilitiesFile.write("\n")
+       # capabilitiesFile.write(capability)
+       # capabilitiesFile.write("\n")
 
-#pull the running config as xml string
+#capabilitiesFile.close()       
+
+#  pull the running config as xml string
 pulledconfig=m.get_config(source='running').xml
 
-#pull a yang schema as a string
-schema = m.get_schema('ietf-interfaces').xml
+#  pull a yang schema as a string and printing to file
+schema = m.get_schema('iana-if-type').xml
+schemaFile = open('schema.txt','w')
+schemaFile.write(schema)
+schemaFile.close()
 
-# Printing some specific items using test filters.
-# Also using XMLtodict to grab the hostname as a string and saving in variable for later use
+#  Printing some specific items using test filters.
+#  Also using XMLtodict to grab the hostname as a string and saving in variable for later use
 
 hostname = """
 <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
@@ -54,10 +67,10 @@ interfaceParseTest = m.get_config(source='running',filter=("subtree",interfaceCo
 print(type(interfaceParseTest))
 print(interfaceParseTest)
 
-# This part of the code is not working. ET cant parse the string with the Unicode element attached to the string. 
-# I can probably solve this by writing to a file and using the ET.parse(filename)
-# I know this is duplication of the config.xml but it is a smaller section and we are practicing working with XML.
-# I do need to do some more research and see if there is a better way to work with this. I am sure xmltodict can handle the parse, will test with that later
+#  This part of the code is not working. ET cant parse the string with the Unicode element attached to the string. 
+#  I can probably solve this by writing to a file and using the ET.parse(filename)
+#  I know this is duplication of the config.xml but it is a smaller section and we are practicing working with XML.
+#  I do need to do some more research and see if there is a better way to work with this. I am sure xmltodict can handle the parse, will test with that later
 
 #interfaceParseTestXML = ET.fromstring(interfaceParseTest)
 #print(type(interfaceParseTestXML))
@@ -67,7 +80,7 @@ print(interfaceParseTest)
 #        print(interface)
 
 
-#Converting the XML string to a dictionary object and printing out the individual configured interfaces and associated settings.
+#  Converting the XML string to a dictionary object and printing out the individual configured interfaces and associated settings.
 
 print('\n' * 5, "*" * 100)
 print(f'      List of interfaces configurations on {routerHostnameValue} : \n')
@@ -87,16 +100,51 @@ for interface in interfaceParseTestDict['rpc-reply']['data']['interfaces']['inte
                 print('IP Address : No IP address configured')
         print('*' * 100,'\n')
 
+#  Section for writing config
+
+#  Filter for writing config
+writeLoopbackInterfaceFilter = '''
+<config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+	<interface>
+          <name>Loopback69</name>
+       	  <name>Loopback69</name>
+	  <description>Created by Badrobot using ncclient</description>	  
+	  <enabled>true</enabled>	  
+	</interface>
+  </interfaces>
+</config>
+  '''
+
+#  Write config to router
+
+m.edit_config(
+        target='running',
+        config=writeLoopbackInterfaceFilter,
+        default_operation='merge'
+)
+
+
+
+
+# setting filter for interface state
+
+interfaceStateFilter = '''
+<interfaces-state xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+</interfaces-state>
+'''
+#  Grabbing the running inteface state, converting to xmlminidom and print to screen
+#  .get fails with no filter, the manager times out waiting for a response.
+
+deviceStatusRawStr = m.get(filter=("subtree",interfaceStateFilter)).xml
+deviceStatusDOM = MiniDom.parseString(deviceStatusRawStr)
+print(deviceStatusDOM.toprettyxml(indent="  -"))
+
+
 m.close_session()
 
-#write the running config that we pulled with .get_config to file on line 11
+#  write the running config that we pulled with .get_config to file on line 11
+
 xmlOutputFile =open('config.xml','w')
 xmlOutputFile.write(pulledconfig)
 xmlOutputFile.close()
-
-
-#cleaning up the schema xml object the we pulled with .get_shema on line 12 and printing to file
-schemaFile = open('schema.txt','w')
-schemaFile.write(schema)
-schemaFile.close()
-
--- XMLtesting.py
+++ XMLtesting.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as ET
+import xmltodict
+import xml.dom.minidom as MINIDom
 from inspect import getmembers, isclass, isfunction
 
 # Display classes in ET module
@@ -21,9 +23,49 @@ for (name,member) in getmembers(ET, isfunction):
 
 print("\n","*" * 50,"\n") 
 
+# Playing around wit the config XML file pulled from the netconf script
+
 getConfigResultsTree = ET.parse('config.xml')
 root= getConfigResultsTree.getroot()
 
 print(root)
 print(type(root))
 print(root.attrib)
+
+print("\n","*" * 50,"\n") 
+
+# Display classes in xmltodict module
+
+print("\n","*" * 50,"\n") 
+print("List of classes : ","\n")
+
+for (name,member) in getmembers(xmltodict, isclass):
+    if not name.startswith("_"):
+        print(name)
+
+# Display functions in xmltodict module
+
+print("\n","*" * 50,"\n") 
+print("List of functions : ","\n")
+
+for (name,member) in getmembers(xmltodict, isfunction):
+    if not name.startswith("_"):
+        print(name)
+
+# Display classes in xml.dom.minidom module
+
+print("\n","*" * 50,"\n") 
+print("List of classes : ","\n")
+
+for (name,member) in getmembers(MINIDom, isclass):
+    if not name.startswith("_"):
+        print(name)
+
+# Display functions in xml.dom.minidom module
+
+print("\n","*" * 50,"\n") 
+print("List of functions : ","\n")
+
+for (name,member) in getmembers(MINIDom, isfunction):
+    if not name.startswith("_"):
+        print(name)
